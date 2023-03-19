a41d775831732aef244d9612775694bf3e4f2a41
Ishvini Anuraj <78601332+Ishvini29@users.noreply.github.com>
2023-03-19 09:21:47 +0530

    serverupdate

--- models/chapter.model.js
+++ models/chapter.model.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 const Chapter = new mongoose.Schema(
   {
-    chapterName: { type: String, required: true, unique: true },
-    depID: { type: String },
+    chaptername: { type: String, required: true, unique: true },
+    depID: { type: Schema.Types.ObjectId, ref: "DepartmentData" },
     createdBy: { type: String, required: true },
     createdOn: { type: String, default: Date.now },
     offeredInJobTitles: { type: String },
     reasons: [{ type: Object }],
+    requested: [{ type: Schema.Types.ObjectId, ref: "UserData" }],
+    accepted: [{ type: Schema.Types.ObjectId, ref: "UserData" }]
   },
   {
     collection: "chapters",
--- models/jobtitle.model.js
+++ models/jobtitle.model.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 const Jobtitle = new mongoose.Schema(
   {
     jobTitlename: { type: String, required: true, unique: true },
+
     depID: { type: String },
     createdBy: { type: String, required: true },
     createdOn: { type: String, default: Date.now },
     chaptersAllocated: { type: String },
+    // chaptersAllocated: { type: Schema.Types.ObjectId, ref: "ChapterData" },
     reasons: [{ type: Object }],
   },
   {
