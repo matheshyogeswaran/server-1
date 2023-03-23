feca10d5879d10fd7787b4451a2db31abf393c6b
Ishvini Anuraj <78601332+Ishvini29@users.noreply.github.com>
2023-03-23 20:04:50 +0530

    chaptermodel

--- models/chapter.model.js
+++ models/chapter.model.js
@@ -6,11 +6,9 @@ const Chapter = new mongoose.Schema(
     depID: { type: mongoose.Types.ObjectId, ref: "DepartmentData" },
     createdBy: { type: mongoose.Types.ObjectId, ref: "UserData" },
     createdOn: { type: Date, default: Date.now },
-<<<<<<< HEAD
+
     offeredInJobTitles: { type: String }, //array objectId from DepartmentData
-=======
-    offeredInJobTitles: { type: String },//depat-id ref 
->>>>>>> 5c588735a6c8b1834a4660332e1723ddccded345
+
     reasons: [{ type: Object }],
     status: { type: String, default: "active" },
     // allocatestatus: { type: String, default: "notallocate" },
