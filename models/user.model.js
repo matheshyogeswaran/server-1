32dd124cc23ac604cb7feba188d14cc980a48bc4
sagini18 <saginisaju@gmail.com>
2023-06-09 23:14:48 +0530

    submission table

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const User = new mongoose.Schema(
   {
-    userRole: { type: String },
+    userRole: { type: mongoose.Schema.Types.ObjectId, ref: "UserRoleData" },
     empId: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
@@ -13,12 +13,18 @@ const User = new mongoose.Schema(
     verified: { type: Boolean, default: false },
     userStatus: { type: String, default: "active" },
     userImage: { type: String },
-    department: { type: mongoose.Schema.Types.ObjectId, ref: "DepartmentData", },
-    jobPosition: { type: mongoose.Schema.Types.ObjectId, ref: "DepartmentData", },
-    badges: [{ badgeValue: { type: String }, earnedOn: { type: Date }, },],
-    earnedScoresByQuiz: { numOfQuizzesDone: { type: Number }, totalScoresEarned: { type: Number }, },
+    department: { type: mongoose.Schema.Types.ObjectId, ref: "DepartmentData" },
+    jobPosition: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "DepartmentData",
+    },
+    badges: [{ badgeValue: { type: String }, earnedOn: { type: Date } }],
+    earnedScoresByQuiz: {
+      numOfQuizzesDone: { type: Number },
+      totalScoresEarned: { type: Number },
+    },
     isProjectRequested: { type: Boolean, default: false },
-    acceptedAdditionalChapter:[{ type: mongoose.Schema.Types.ObjectId }],
+    acceptedAdditionalChapter: [{ type: mongoose.Schema.Types.ObjectId }],
   },
   {
     collection: "users",
